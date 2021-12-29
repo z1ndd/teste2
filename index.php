d8a4d597aab0e83bf886379c6e3ee8dc9e1a11a1
z1ndd <95886051+z1ndd@users.noreply.github.com>
2021-12-29 13:57:49 +0000

    Home

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>FSJoalharia</title>
 </head>
 <body>
     <?php include "./html/navbar.html"; ?>
 
-    bananas
+    <?php include "./html/home.html"; ?>
     
     <?php //include "./footer.html"; ?>
 </body>
