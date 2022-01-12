7892c654c1e951410e8a67ed3f6226ad88c898cb
z1ndd <95886051+z1ndd@users.noreply.github.com>
2022-01-12 18:48:05 +0000

    lista

--- php/listaUtilizadores.php
+++ php/listaUtilizadores.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-	<title></title>
+	<title>Lista Utilizadores</title>
 
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -16,7 +16,6 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
 </head>
 
 <body>
