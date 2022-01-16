8c5855cdb537d2fdfeaf3b7ace43edf51b117198
z1ndd <95886051+z1ndd@users.noreply.github.com>
2022-01-16 05:46:16 +0000

    inserir fixed

--- php/insere-dados.php
+++ php/insere-dados.php
@@ -2,17 +2,16 @@
 
 //receber os valores que são enviados pelo formulário de registo de utilizador
 
-$id = $_GET['iduser'];
+//$id = $_GET['iduser'];
 $nome = $_POST['nome'];
 $login = $_POST['login'];
 $email = $_POST['email'];
-$password = $_POST['pass'];
+$password = $_POST['password'];
 
 //incluir o ficheiro de ligação à base de dados, para que a variável $liga possa ser utilizada
 include "ligacaoBD.php";
 
 //construção da query de inserção do registo na base de dados
-#$query = "INSERT INTO utilizador (email, login, nome, password) VALUES ('$email', '$login', '$nome', '$password')";
 $query = "INSERT INTO utilizador (iduser, email, login, nome, password) VALUES (NULL, '$email', '$login', '$nome', '$password')";
 
 //se a query estiver correta, executa e mostra mensagem de registo adicionado
@@ -20,7 +19,6 @@ if(mysqli_query($liga,$query))
 {
 	$mesg = "Registo Alterado com Sucesso";
 	echo "<script>alert('$mesg')</script>";
-	/* header("location: listaUsers.php"); */
 	echo "<a href='lista-utilizador.php'>voltar</a>";
 }
 else
@@ -30,19 +28,4 @@ else
 //no final deve ser desligada a ligação
 mysqli_close($liga);
 
-?>
-<?php
-/* executar a consulta e testar se ocorreu erro */
-if(!mysqli_query($liga,$query))
-{
-    echo "Erro: ".$query."<br>".mysqli_error($liga);
-    mysqli_close($liga);  /* fechar a ligação */
-}
-else{
-    /* percorrer os registos (linhas) da tabela e mostrar na página */
-    echo " Novo registo inserido com sucesso" ;
-
-    }
-
-	mysqli_close($liga);        /* fechar a ligação */
 ?>
--- php/inserir-utilizador.php
+++ php/inserir-utilizador.php
@@ -4,24 +4,8 @@
         include('../elements/navbar-admin.php'); ?>
 	<!-- inserir user --> 
 <body>
-	
-	<?php
-	/*
-	if (!isset($_GET['id'])) {
-		header("location: lista-utilizador.php");
-	} else {
-		$id = $_GET['id'];
-	} 
-	include "ligacaoBD.php"; 
-
-	$query = "SELECT * FROM utilizador WHERE iduser =$id";
-	$resultado = mysqli_query($liga, $query);
-	if (mysqli_num_rows($resultado) > 0) {
-		$row = mysqli_fetch_assoc($resultado);
-*/
-	?>
-	
-	<form class="col-md-6 w-50" style="margin-left:25%" action="insere-dados.php?id=<?php echo $row['iduser']; ?>" method="POST" id="form1">
+		
+	<form class="col-md-6 w-50" style="margin-left:25%" action="insere-dados.php" method="POST" id="form1">
 		<div class="form-group">
 			<label for="nome" style="text-align: left;">Nome</label>
 			<input type="text" name="nome" class="form-control" id="nome" placeholder="Insira o seu nome" minlength="8" maxlength="45" required>
@@ -32,7 +16,7 @@
 		</div>
 		<div class="form-group">
 			<label for="mail">Email</label>
-			<input type="email" name="mail" class="form-control" id="mail" placeholder="Insira o seu email" required>
+			<input type="email" name="email" class="form-control" id="mail" placeholder="Insira o seu email" required>
 		</div>
 		<div class="form-group">
 			<label for="pass">Password</label>
@@ -45,21 +29,4 @@
 		<center><br><button type="submit" class="btn btn-primary">Inserir Dados</button></center>
 
 	</form>
-
-	<?php
-	/*
-	} else {
-		echo "Não há resultados";
-	}
-*/
-	?>
-	<script>
-		$("#form1").validate({
-			rules: {
-				repass: {
-					equalTo: "#pass"
-				}
-			}
-		});
-	</script>
 </body>
